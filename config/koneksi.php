cda536d879528880fd7733a89d7f201576de3e51
davi <davinovan.2024@gmail.com>
2026-08-11 16:28:02 +0700

    ubah koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "db_lib";
+$db   = "db_library";
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) die("Koneksi gagal: " . mysqli_connect_error());
 mysqli_set_charset($koneksi, "utf8mb4");
